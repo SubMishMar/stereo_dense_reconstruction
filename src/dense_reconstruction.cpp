c25e5e4e0062df1787bcd81381c68e545a400629
SubMishMar <subodheenitr@gmail.com>
2019-07-25 01:51:49 -0500

    working on kitti dataset

--- src/dense_reconstruction.cpp
+++ src/dense_reconstruction.cpp
@@ -1,8 +1,14 @@
 #include <ros/ros.h>
+
+#include <pcl/common/common.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
-#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/ChannelFloat32.h>
 #include <geometry_msgs/Point32.h>
 #include <image_transport/image_transport.h>
@@ -29,15 +35,11 @@ image_transport::Publisher dmap_pub;
 ros::Publisher pcl_pub;
 
 void publishPointCloud(Mat& img_left, Mat& dmap) {
-
+  pcl::PointCloud<pcl::PointXYZRGB> out_cloud_pcl;
   Mat V = Mat(4, 1, CV_64FC1);
   Mat pos = Mat(4, 1, CV_64FC1);
   vector< Point3d > points;
-  sensor_msgs::PointCloud pc;
-  sensor_msgs::ChannelFloat32 ch;
-  ch.name = "rgb";
-  pc.header.frame_id = "jackal";
-  pc.header.stamp = ros::Time::now();
+
   for (int i = 0; i < img_left.cols; i++) {
     for (int j = 0; j < img_left.rows; j++) {
       int d = dmap.at<uchar>(j,i);
@@ -55,24 +57,17 @@ void publishPointCloud(Mat& img_left, Mat& dmap) {
       double X = pos.at<double>(0,0) / pos.at<double>(3,0);
       double Y = pos.at<double>(1,0) / pos.at<double>(3,0);
       double Z = pos.at<double>(2,0) / pos.at<double>(3,0);
-      Mat point3d_cam = Mat(3, 1, CV_64FC1);
-      point3d_cam.at<double>(0,0) = X;
-      point3d_cam.at<double>(1,0) = Y;
-      point3d_cam.at<double>(2,0) = Z;
-      // transform 3D point from camera frame to robot frame
-      Mat point3d_robot = XR * point3d_cam + XT;
-      points.push_back(Point3d(point3d_robot));
-      geometry_msgs::Point32 pt;
-      pt.x = point3d_robot.at<double>(0,0);
-      pt.y = point3d_robot.at<double>(1,0);
-      pt.z = point3d_robot.at<double>(2,0);
-      pc.points.push_back(pt);
       int32_t red, blue, green;
       red = img_left.at<Vec3b>(j,i)[2];
       green = img_left.at<Vec3b>(j,i)[1];
       blue = img_left.at<Vec3b>(j,i)[0];
-      int32_t rgb = (red << 16 | green << 8 | blue);
-      ch.values.push_back(*reinterpret_cast<float*>(&rgb));
+
+      pcl::PointXYZRGB pt(red, green, blue);
+
+      pt.x = Z;
+      pt.y = -X;
+      pt.z = -Y;
+      out_cloud_pcl.push_back(pt);
     }
   }
   if (!dmap.empty()) {
@@ -80,8 +75,11 @@ void publishPointCloud(Mat& img_left, Mat& dmap) {
     disp_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", dmap).toImageMsg();
     dmap_pub.publish(disp_msg);
   }
-  pc.channels.push_back(ch);
-  pcl_pub.publish(pc);
+  sensor_msgs::PointCloud2 out_cloud_ros;
+  pcl::toROSMsg(out_cloud_pcl, out_cloud_ros);
+  out_cloud_ros.header.frame_id = "left_camera";
+  out_cloud_ros.header.stamp = ros::Time::now();
+  pcl_pub.publish(out_cloud_ros);
 }
 
 Mat generateDisparityMap(Mat& left, Mat& right) {
@@ -102,33 +100,37 @@ Mat generateDisparityMap(Mat& left, Mat& right) {
 }
 
 void imgCallback(const sensor_msgs::ImageConstPtr& msg_left, const sensor_msgs::ImageConstPtr& msg_right) {
-  Mat tmpL = cv_bridge::toCvShare(msg_left, "mono8")->image;
-  Mat tmpR = cv_bridge::toCvShare(msg_right, "mono8")->image;
-  if (tmpL.empty() || tmpR.empty())
+  Mat tmpL_color = cv_bridge::toCvShare(msg_left, sensor_msgs::image_encodings::BGR8)->image;
+  Mat tmpR_color = cv_bridge::toCvShare(msg_right, sensor_msgs::image_encodings::BGR8)->image;
+
+  if (tmpL_color.empty() || tmpR_color.empty())
     return;
-  
+
+  cv::Mat tmpL, tmpR;
+  cvtColor(tmpL_color, tmpL, CV_BGR2GRAY);
+  cvtColor(tmpR_color, tmpR, CV_BGR2GRAY);
   Mat img_left, img_right, img_left_color;
   remap(tmpL, img_left, lmapx, lmapy, cv::INTER_LINEAR);
   remap(tmpR, img_right, rmapx, rmapy, cv::INTER_LINEAR);
-  
-  cvtColor(img_left, img_left_color, CV_GRAY2BGR);
-  
+
   Mat dmap = generateDisparityMap(img_left, img_right);
-  publishPointCloud(img_left_color, dmap);
+  publishPointCloud(tmpL_color, dmap);
   
-  imshow("LEFT", img_left);
-  imshow("RIGHT", img_right);
+  imshow("LEFT", tmpL_color);
+  imshow("RIGHT", tmpR_color);
   imshow("DISP", dmap);
   waitKey(30);
 }
 
-void findRectificationMap(FileStorage& calib_file, Size finalSize) {
+void findRectificationMap() {
   Rect validRoi[2];
   cout << "Starting rectification" << endl;
   stereoRectify(K1, D1, K2, D2, calib_img_size, R, Mat(T), R1, R2, P1, P2, Q, 
-                CALIB_ZERO_DISPARITY, 0, finalSize, &validRoi[0], &validRoi[1]);
-  cv::initUndistortRectifyMap(K1, D1, R1, P1, finalSize, CV_32F, lmapx, lmapy);
-  cv::initUndistortRectifyMap(K2, D2, R2, P2, finalSize, CV_32F, rmapx, rmapy);
+                CALIB_ZERO_DISPARITY, 0, calib_img_size, &validRoi[0], &validRoi[1]);
+  cv::initUndistortRectifyMap(K1, D1, R1, P1, calib_img_size, CV_32F, lmapx, lmapy);
+
+  cv::initUndistortRectifyMap(K2, D2, R2, P2, calib_img_size, CV_32F, rmapx, rmapy);
+
   cout << "Done rectification" << endl;
 }
 
@@ -159,9 +161,13 @@ int main(int argc, char** argv) {
 
   int calib_width, calib_height, out_width, out_height;
 
+  calib_width =
+          readParam<int>(nh, "calib_width");
+  calib_height =
+          readParam<int>(nh, "calib_height");
+
   calib_img_size = Size(calib_width, calib_height);
-  out_img_size = Size(out_width, out_height);
-  
+
   calib_file = FileStorage(calib_file_name, FileStorage::READ);
   calib_file["K1"] >> K1;
   calib_file["K2"] >> K2;
@@ -171,18 +177,18 @@ int main(int argc, char** argv) {
   calib_file["T"] >> T;
   calib_file["XR"] >> XR;
   calib_file["XT"] >> XT;
+
+  findRectificationMap();
   
-  findRectificationMap(calib_file, out_img_size);
-  
-  message_filters::Subscriber<sensor_msgs::Image> sub_img_left(nh, left_img_topic, 1);
-  message_filters::Subscriber<sensor_msgs::Image> sub_img_right(nh, right_img_topic, 1);
+  message_filters::Subscriber<sensor_msgs::Image> sub_img_left(nh, left_img_topic, 100);
+  message_filters::Subscriber<sensor_msgs::Image> sub_img_right(nh, right_img_topic, 100);
   
   typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> SyncPolicy;
   message_filters::Synchronizer<SyncPolicy> sync(SyncPolicy(10), sub_img_left, sub_img_right);
   sync.registerCallback(boost::bind(&imgCallback, _1, _2));
 
   dmap_pub = it.advertise("/camera/left/disparity_map", 1);
-  pcl_pub = nh.advertise<sensor_msgs::PointCloud>("/camera/left/point_cloud",1);
+  pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("/camera/left/point_cloud",1);
 
   ros::spin();
   return 0;
